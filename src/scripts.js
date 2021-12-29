dea97048e51de89e3115d2f5b0ea4d1244c06842
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-12-28 19:18:24 -0800

    updates on scripts

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,7 @@ function pushToBuild(pushOBJ) {
   execSync(`rm -r ../${pushOBJ.repoName}`);
   execSync(`mkdir ../${pushOBJ.repoName}`)
   execSync(`git clone ${pushOBJ.gitURL} ../${pushOBJ.repoName}`);
-  execSync(`npm install --prefix ../${pushOBJ.repoName}`);
+  //execSync(`npm install --prefix ../${pushOBJ.repoName}`);
   execSync(`pm2 start ../${pushOBJ.repoName}/${pushOBJ.main_file}`);
 }
 
