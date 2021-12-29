9b63be53f8ae638aeda6ced572696ec4512f30f9
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-12-28 19:09:22 -0800

    updates on scripts

--- src/scripts.js
+++ src/scripts.js
@@ -2,9 +2,10 @@ const { execSync } = require("child_process");
 
 function pushToBuild(pushOBJ) {
   execSync(`rm -r ../${pushOBJ.repoName}`);
-  execSync(`git clone ${pushOBJ.gitURL} ../`);
-  execSync(`npm install --prefix ../${pushOBJ.repoName}`);
-  execSync(`pm2 start ../${pushOBJ.repoName}/${pushOBJ.main_file}`);
+  execSync(`mkdir ../${pushOBJ.repoName}`)
+  execSync(`git clone ${pushOBJ.gitURL} ../${pushOBJ.repoName}`);
+  execSync(`npm install --prefix ../${pushOBJ.repoName}/${pushOBJ.repoName}`);
+  execSync(`pm2 start ../${pushOBJ.repoName}/${pushOBJ.repoName}/${pushOBJ.main_file}`);
 }
 
 module.exports = { pushToBuild };
